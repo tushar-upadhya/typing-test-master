afdb94f2bf082a52699a751361086c6b5df60d47
Tushar Upadhyay <tusharupadhyay691@gmail.com>
2023-06-24 17:31:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,336 @@
+let paragraph = [
+    `We learned to see, at long last. Partway through the millennium,we figured out how to shape and polish glass so as to see far andto see small, and we dug into dead languages of previousmillenniums to name our new aids to seeing-telescope,
+    microscope, spectroscope, spectrophotometer, spectroheliograph
+    and, eventually, television. We figured out the art, the geometry
+    and the semantics of perspective. No wonder our superheroes had
+    X-ray vision; so did we. And infrared vision, and ultraviolet vision,
+    and gamma-ray vision and nuclear-magnetic-resonant vision. We
+    extended our sight far beyond the tiny spectrum our unaided eyes
+    could handle, from violet to red. We looked out, and we looked
+    inside. We saw where earth is and what humans are (if not quite
+    who we are). We noticed quasars and we noticed viruses.
+    Surprise! "In all falling rain, carried from gutters into water-butts,
+    animalcules are to be found. in all kinds of water, standing in the
+    open air, animalcules can turn up," noted Anton van
+    Leeuwenhoek, the first man to observe bacteria. We figured out
+    some things about color and space.`,
+    `Of the many things that the 2020 Delhi assembly election
+    heralded, one that stands out because of its optics is the public
+    emergence of Hanuman bhakt Arvind Kejriwal. In his victory
+    speech on Tuesday, Arvind Kejriwal thanked Lord Hanuman for
+    blessing Delhi and said the mandate given to the Aam Aadmi
+    Party (AAP) has marked the dawn of a "new form of politics" in
+    India. In Kejriwal's words, this newness is outlined by "kaam ki
+    rajneeti (politics of work)" wherein people would vote on the basis
+    of developmental works (schools, hospitals, affordable healthcare,
+    electricity etc) carried out by a government. "This is an auspicious
+    message for the country," he said. No doubt, that's refreshing to
+    hear, especially in a political atmosphere vitiated by a high-decibel
+    cacophony of the worst order. But development was not the only
+    axis on which Arvind Kejriwal's Delhi election campaign rotated on.
+    To mellow down the vitriol of BJP's hard Hindutva politics married
+    to its brand of nationalism, Arvind Kejriwal rebranded himself as a
+    devout Hanuman bhakt who isn't shy of wearing his religious
+    identity down his sleeves; say 'Jai Hind' and 'Vande Matram';
+    carefully side-step thorny uncomfortable issues like CAA, NRC
+    and Shaheen Bagh; `,
+    `How much policy of reservation is successful in our country? The
+    main object of reservation is to uplift the poor and the
+    down-trodden. But in reality, the experience of the past more than
+    half a century shows that the policy of reservation has miserably
+    failed to achieve its objectives. It is far from promoting harmony
+    and spirit of brotherhood among the people, which is the goal of
+    our Constitution. It is appreciable that financial help and other
+    assistance are being given to the needy through the policy of
+    reservation. But reality is that the policy of improving their
+    conditions has made them dependent on crutches which mar their
+    self-confidence. On the other hand, policy of reservation
+    perpetuates injustice and disservice to the nation, because merit is
+    thrown to the winds and its place is taken by inefficiency. In the
+    light of the experience of reservation during the long span of more
+    than half a century, it is quite evident that if the present policy of
+    reservation is continued, the conditions of Scheduled castes and
+    Scheduled Tribes will not improve much and up to the desired
+    target, because it is absolutely not a right solution to improve their
+    living standard. `,
+    `A soldier by temperament, Maharaja Ranjit Singh was also a lover
+    of the arts. Apart from his numerous adventures, big and small, he
+    was always kept mentally preoccupied. The only relaxation he
+    indulged in was a long ride on one of his favourite horses or to
+    witness dance & music. And yet his admirers make a fairly
+    convincing case that the Maharaja loved the arts and endeavoured
+    to promote artistic activity in his regime. What appears to be more
+    significant? is Maharaja Ranjit Singh's unmistakably secular
+    approach in matters pertaining to the arts, whether it was painting
+    or architecture, music or dance. When Maharaja annexed Kangra
+    and other hill states, the people of Punjab generally and Lahore
+    darbar in particular came in touch with the glorious heritage of
+    Pahari artists, who were patronised without any consideration of
+    caste and creed by the Maharaja, his household and even his
+    courtiers. A number of them are said to have entered into the
+    service of the Maharaja or his nobles. According to Mr. B.N.
+    Goswamy, an eminent art critic, some of the artists may be named
+    as Nikka, Gokal, Harkhu, Chhajju and Damodar, all of Rajol family
+    of artists, that we know so well. Maharaja Ranjit Singh's regime is
+    better known for its pro-motion of the arts of portraiture.`,
+    `Before he launched the most viral video in Internet history, Jason
+    Russell was a half-hearted Web presence. His YouTube account
+    was dead, and his Facebook and Twitter pages were a trickle of
+    kid pictures and home-garden updates. The Web wasn't made "to
+    keep track of how much people like us," he thought, and when his
+    own tech habits made him feel like "a genius, an addict, or a
+    megalomaniac," he unplugged for days, believing, as the humorist
+    Andy Borowitz put it in a tweet that Russell tagged as a favorite,
+    "it's important to turn off our computers and do things in the real
+    world." But this past March Russell struggled to turn off anything.
+    He forwarded a link to "Kony 2012," his deeply personal Web
+    documentary about the African warlord Joseph Kony. The idea
+    was to use social media to make Kony famous as the first step to
+    stopping his crimes. And it seemed to work: the film hurtled
+    through cyberspace, clocking more than 70 million views in less
+    than a week. But something happened to Russell in the process.
+    The same digital tools that supported his mission seemed to tear
+    at his psyche, exposing him to nonstop kudos and criticisms, and
+    ending his arm's-length relationship with new media.`,
+    `My grandmother, like everybody's grandmother was an old
+    woman, she had been old and wrinkled for the twenty years that I
+    had known her. People said that she had once been young and
+    pretty and even had a husband. But that was hard to believe. My
+    grandfather's portrait hung above the mantelpiece in the
+    drawing-room. He wore big turban and loose-fitting clothes. His
+    long, white beard covered the best part of his chest and he looked
+    at least a hundred years. His long, white beard covered the best
+    part of his chest and he looked at least a hundred years old. He
+    did not look the sort of person who would have a wife or children.
+    He looked as if could only l have lots and lots of grandchildren. As
+    for my grandmother being young and pretty, the thought was
+    almost revolting. She often told us of the games she used to play
+    as a child. That seemed quite absurd and undignified on her part
+    and we treated it like the fables of the Prophets she used to tell us.
+    She had always been short and fat slightly bent. Her face was a
+    crisscross of wrinkles running from everywhere to everywhere. No,
+    we were certain she had always been as we had known her. Old,
+    so terribly old that she could not have grown older and had stayed
+    at the same age for twenty years. `,
+    `It is the relative weight which we give to each of these reasons that
+    decides what sort of discipline we have. And that can vary from the
+    pure self-discipline of the of the Sermon on the Mount to the
+    discipline of the concentration camp the enforced discipline of fear.
+    Inspite of all our squabbles the British are united when it comes to
+    most of the things that mater and liberty is one them. We believe in
+    freedom to think what we like say what we like work at what we
+    like and go where we like. Discipline is restraint on liberty so many
+    of us have a very natural inclination to avoid it. But we cannot.
+    Man ever since the dim prehistoric past has had no option but to
+    accept the discipline of some kind For a modern man living in
+    complex communities is more than even unavoidable. All history
+    teaches that when through either idleness weakness or faction the
+    sense of fades in a nation its economic life sinks into decay then
+    as its standard of living falls and security vanishes one of two
+    things happens. Either some more virile militant power steps in to
+    impose its own brand of discipline or a dictator arises and clamps
+    down the iron control of the police state. Somehow eventually
+    discipline is again enforced. `,
+    `Ken Liu's incredible story "Paper Menagerie" just became the first
+    work of fiction to win all three of SF's major awards: the Hugo, the
+    Nebula and the World Fantasy Award. And we're proud to be able
+    to reprint the whole story, right here at io9. Here's your chance to
+    find out what all the excitement is about, and discover one of
+    science fiction's fastest rising stars. One of my earliest memories
+    starts with me sobbing. I refused to be soothed no matter what
+    Mom and Dad tried. Dad gave up and left the bedroom, but Mom
+    took me into the kitchen and sat me down at the breakfast table.
+    "Kan, kan," she said, as she pulled a sheet of wrapping paper from
+    on top of the fridge. For years, Mom carefully sliced open the
+    wrappings around Christmas gifts and saved them on top of the
+    fridge in a thick stack. She set the paper down, plain side facing
+    up, and began to fold it. I stopped crying and watched her, curious.
+    She turned the paper over and folded it again. She pleated,
+    packed, tucked, rolled, and twisted until the paper disappeared
+    between her cupped hands. Then she lifted the folded-up paper
+    packet to her mouth and blew into it, like a balloon. "Kan," she
+    said. "Laohu." `,
+    `It was one of those perfect southern California afternoons, the sun
+    peeking through a thick grove of trees, the air just a touch
+    autumnal. I was eating lunch with the novelist TC Boyle in the
+    backyard of his Frank Lloyd Wright-designed house in Montecito,
+    the posh enclave of Santa Barbara in California where he has lived
+    for more than two decades, raising three children with his wife of
+    42 years. His black puli, Ilka, played happily on the deck. "It's quite
+    clear to me that our species is on the way out," Boyle said
+    casually, as if the scene had grown just a little too suburban. It is
+    the novelist's job, after all, to jostle us out of our comforts, while
+    being entertaining - that is, to make discomfort pleasurable. Boyle
+    has proved remarkably capable in this regard through his 26 works
+    of fiction, which combine the zany humour of early Woody Allen
+    with An Inconvenient Truth's concern for our collective fate.
+    Boyle's latest novel, The Terranauts: A Novel, is set in the desert
+    of Arizona - which, with global warming on the rise, much of
+    California is starting to resemble. It is the story of a Biosphere
+    2-like experiment to prepare for the colonisation of space, giving
+    humanity the chance to ruin another planet with Starbucks-laden
+    strip malls.`,
+    `The journey, which I remember well, was very pleasant. I made
+    friends with many people on the train. One lady gave me a box of
+    shells. My father made holes in these so that I could string them,
+    and for a long time they kept me happy and contented. The
+    conductor, too, was kind. Often when he went his rounds I clung to
+    his coat tails while he collected and punched the tickets. His
+    punch, with which he let me play, was a delightful toy. Curled up in
+    a corner of the seat I amused myself for hours making funny little
+    holes in bits of cardboard. My aunt made me a big doll out of
+    towels. It was the most comical shapeless thing, this improvised
+    doll, with no nose, mouth, ears or eyesÂ­nothing that even the
+    imagination of a child could convert into a face. Curiously enough,
+    the absence of eyes struck me more than all the other defects put
+    together. I pointed this out to everybody with provoking
+    persistency, but no one seemed equal to the task of providing the
+    doll with eyes. A bright idea, however, shot into my mind, and the
+    problem was solved. I tumbled off the seat and searched under it
+    until I found my aunt's cape, which was trimmed with large beads. I
+    pulled two bead off and indicated to her that I wanted her to sew
+    them on my doll.`,
+    `Most of our present day youth are agitational and irreverent. This
+    is almost a general complaint. Elders also exclaim, "What a
+    change in our times, it was so different. We respected our elders
+    and teachers. Even now when we pass by them, we touch their
+    feet. Now, impression has gained currency that modern
+    youngsters have adopted a careless attitude." This impression is
+    sought to be supported by the youngsters' air of freedom, his
+    trendy dress, smoking, drugs, drinking, late hours mixing with
+    members of the opposite sex and agitations on college and
+    university campus. In other words, Society has a new breed of
+    youngsters out to. destroy its fabric. The youngsters defence
+    indifference and contempt of established norm - is all to0 obvious.
+    The youngster bristles with impatience. This is observed in the
+    home, class-room, gathering and at every place where he comes
+    in contact with elders. Sometimes, he speaks to teachers in a tone
+    less than respectful which is being tolerated as a mark of poor
+    breeding, bad manners and lack of culture in the present youth. At
+    the same time, if a youngster puts across his view point effectively,
+    it does not mean that he shows disrespect to authority at the
+    university, home or elsewhere. `,
+    `The machine age gives us year by year more hours of leisure but it
+    fails to teach us how to use them. It gives us mechanical habits of
+    mind and represses the spirit to adventure-except along
+    machine-made lines. We will need all our creative powers to think
+    our way our of the social problems which science has created for
+    us. It is science that has given us the unexpected redistribution of
+    the age groups. Almost every year, some modem drug adds a little
+    more to the average span of life, until the upper group is
+    overcrowded. In the United States, for instance, there are already
+    nine million people 1950's. In fifteen years' time, this number will
+    reach the astonishing figure of forty-five million. Who is to keep
+    them? It will need some readjustment. And so, science goes on
+    raising its problems. Compared with our fundamental
+    question-What is Life? These problems may seem to be of less
+    importance. But they are not really so. What is happening is that
+    science is creating problems faster than they can be solved. Man
+    is struggling in a sort of vicious circle, always striving to catch up
+    and never getting nearer. And there are no signs that the glut of
+    discoveries is coming to an end. `,
+];
+
+const input = document.querySelector("input");
+const displayText = document.querySelector(".display p");
+const mis = document.querySelector(".mistake span");
+const cpm = document.querySelector(".cpm span");
+const wpmTag = document.querySelector(".wpm span");
+const time = document.querySelector(".time span b");
+const tryAgain = document.querySelector("button");
+const wrapper = document.querySelector(".wrapper");
+const instruction = document.querySelector(".instruction");
+const gameOver = document.querySelector(".gameOver");
+
+let timer,
+    maxTime = 60,
+    leftTime = maxTime;
+let textIndex = (mistakes = isTyping = 0);
+
+function randomParagraph() {
+    displayText.innerText = "";
+
+    const randomNumber = Math.floor(Math.random() * paragraph.length);
+
+    paragraph[randomNumber].split("").forEach((char) => {
+        let spanTag = `<span>${char}</span>`;
+
+        displayText.innerHTML += spanTag;
+    });
+
+    displayText.addEventListener("click", () => {
+        input.focus();
+        instruction.style.display = "none";
+    });
+}
+
+input.addEventListener("input", () => {
+    const text = document.querySelectorAll("span");
+
+    let typedText = input.value.split("")[textIndex];
+
+    if (textIndex < text.length - 1 && leftTime > 0) {
+        if (!isTyping) {
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
+        }
+
+        if (typedText == null) {
+            textIndex--;
+            if (text[textIndex].classList.contains("incorrect")) {
+                mistakes--;
+            }
+
+            text[textIndex].classList.remove("correct", "incorrect");
+        } else {
+            if (text[textIndex].innerText === typedText) {
+                text[textIndex].classList.add("correct");
+            } else {
+                mistakes++;
+                text[textIndex].classList.add("incorrect");
+            }
+            textIndex++;
+        }
+
+        text.forEach((span) => span.classList.remove("active"));
+        text[textIndex].classList.add("active");
+
+        let wpm = Math.round(
+            ((textIndex - mistakes) / 5 / (maxTime - leftTime)) * 60
+        );
+
+        wpm = wpm < 0 || !wpm || wpm == Infinity ? 0 : wpm;
+        wpmTag.innerText = wpm;
+        mis.innerText = mistakes;
+        cpm.innerText = textIndex - mistakes;
+    } else {
+        input.value = "";
+        clearInterval(timer);
+        gameOver.style.display = "block";
+    }
+});
+
+function initTimer() {
+    if (leftTime > 0) {
+        leftTime--;
+        time.innerHTML = leftTime;
+    } else {
+        input.innerHTML = "";
+        clearInterval(timer);
+    }
+}
+
+tryAgain.addEventListener("click", () => {
+    randomParagraph();
+    gameOver.style.display = "none";
+    instruction.style.display = "block";
+    clearInterval(timer);
+    textIndex = mistakes = isTyping = 0;
+    leftTime = maxTime;
+    time.innerText = leftTime;
+    mis.innerText = 0;
+    wpmTag.innerText = 0;
+    cpm.innerText = 0;
+});
+
+randomParagraph();
